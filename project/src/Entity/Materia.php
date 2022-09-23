1895982dbd748ff1a1e2bd307cfea87455d9e4bd
Maximiliano <maximilianokerps@gmail.com>
2022-09-23 01:45:24 -0300

    Entity created: Materia

---
+++ project/src/Entity/Materia.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MateriaRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=MateriaRepository::class)
+ */
+class Materia
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nombre;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $descripcion;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $dia;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $hora_inicio;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $hora_fin;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $aula;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $estado;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNombre(): ?string
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre(string $nombre): self
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    public function getDescripcion(): ?string
+    {
+        return $this->descripcion;
+    }
+
+    public function setDescripcion(string $descripcion): self
+    {
+        $this->descripcion = $descripcion;
+
+        return $this;
+    }
+
+    public function getDia(): ?string
+    {
+        return $this->dia;
+    }
+
+    public function setDia(string $dia): self
+    {
+        $this->dia = $dia;
+
+        return $this;
+    }
+
+    public function getHoraInicio(): ?int
+    {
+        return $this->hora_inicio;
+    }
+
+    public function setHoraInicio(int $hora_inicio): self
+    {
+        $this->hora_inicio = $hora_inicio;
+
+        return $this;
+    }
+
+    public function getHoraFin(): ?int
+    {
+        return $this->hora_fin;
+    }
+
+    public function setHoraFin(int $hora_fin): self
+    {
+        $this->hora_fin = $hora_fin;
+
+        return $this;
+    }
+
+    public function getAula(): ?string
+    {
+        return $this->aula;
+    }
+
+    public function setAula(string $aula): self
+    {
+        $this->aula = $aula;
+
+        return $this;
+    }
+
+    public function getEstado(): ?string
+    {
+        return $this->estado;
+    }
+
+    public function setEstado(string $estado): self
+    {
+        $this->estado = $estado;
+
+        return $this;
+    }
+}
